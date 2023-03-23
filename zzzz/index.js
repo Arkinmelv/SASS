04c68ae4c3a3d268cfd8598c8442db3664f33aca
04_3C_Arkin Melvian Dwiansyah <arkin667@gmail.com>
2023-03-23 15:24:52 +0700

    arkin-responsive

--- zzzz/index.js
+++ zzzz/index.js
@@ -1,8 +1,23 @@
-
-const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector('.nav-menu');
-
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    navMenu.classList.toggle('active');
-});
+; (function ($) {
+    $(function () {
+        $('nav ul li > a:not(:only-child)').click(function (e) {
+            $(this)
+                .siblings('.nav-dropdown')
+                .slideToggle()
+            $('.nav-dropdown')
+                .not($(this).siblings())
+                .hide()
+            e.stopPropagation()
+        })
+        $('html').click(function () {
+            $('.nav-dropdown').hide()
+        })
+        // Toggle open and close nav styles on click
+        $('#nav-toggle').click(function () {
+            $('nav ul').slideToggle();
+        });
+        $('#nav-toggle').on('click', function () {
+            this.classList.toggle('active')
+        })
+    })
+})(jQuery)
